8900533a2e6a6625be79ad27f1ba5dfaacdd12fa
liltalas <zamirbekovasyrga@gmail.com>
2022-12-02 04:05:38 -0600

    identified the name of the weekday

--- src/main/java/devxschool/student/DayIdentifier.java
+++ src/main/java/devxschool/student/DayIdentifier.java
@@ -23,13 +23,34 @@ public class DayIdentifier {
          *
          */
 
-        Scanner scanner;
-        int input = 0;
-        String day = "";
+        Scanner scanner = new Scanner(System.in);
+        String day = ""; // declaring and initializing
 
         //TODO implement your code here...
+        System.out.print("Enter a number to get day of the week: ");
+        int numOfDay = scanner.nextInt();
+
+       if(numOfDay == 1){
+            day = "Monday";
+        } else if(numOfDay == 2){
+            day = "Tuesday";
+        } else if(numOfDay == 3){
+            day = "Wednesday";
+        } else if(numOfDay == 4){
+            day = "Thursday";
+        } else if(numOfDay == 5){
+            day = "Friday";
+        } else if(numOfDay == 6){
+            day = "Saturday";
+        } else if(numOfDay == 7){
+            day = "Sunday";
+        } else {
+            day = "Wrong Day";
+        }
+
+        System.out.println("You selected " + day + "!");
 
 
-        Validation.testDayIdentifier(input, day);
+        Validation.testDayIdentifier(numOfDay, day);
     }
 }
